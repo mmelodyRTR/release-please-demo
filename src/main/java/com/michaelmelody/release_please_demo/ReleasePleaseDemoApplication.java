80577fdf7dc0bf2e888d9e9bc286a2f7b948ca88
mmelodyRTR <mmelody@renttherunway.com>
2025-03-11 09:41:34 +0000

    1st commit

---
+++ src/main/java/com/michaelmelody/release_please_demo/ReleasePleaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.michaelmelody.release_please_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleasePleaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleasePleaseDemoApplication.class, args);
+	}
+
+}
